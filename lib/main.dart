0419ed026051b4f3acef8fc3a6fd6cecac5b8a21
ˍıWéd Farrāи <wedfarran6@hotmail.com>
2023-11-16 14:54:50 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,37 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MagicBalls());
 }
 
-class MagicBalls extends StatelessWidget {
+class MagicBalls extends StatefulWidget {
   const MagicBalls({super.key});
 
   @override
+  State<MagicBalls> createState() => _MagicBallsState();
+}
+
+class _MagicBallsState extends State<MagicBalls> {
+  int ballNumber = 1;
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Ask me anything'),
+        ),
+        body: InkWell(
+          onTap: () => setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          }),
+          child: Center(
+            child: Image.asset('assets/images/ball$ballNumber.png'),
+          ),
+        ),
+      ),
+    );
   }
 }
