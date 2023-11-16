d6c59ed62e915ab045297d43612565ff8a0cc0e0
ˍıWéd Farrāи <wedfarran6@hotmail.com>
2023-11-16 10:49:11 +0300

    magic 8 balls project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MagicBalls());
+}
+
+class MagicBalls extends StatelessWidget {
+  const MagicBalls({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
